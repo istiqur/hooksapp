9465a6b6f3601cd8aa6729bc591adce3de895ffa
Istiqur Rahman <rabby.istik@gmail.com>
2020-07-13 12:09:38 +0600

    useState in a form

--- src/components/SongList.jsx
+++ src/components/SongList.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import uuid from "uuid/dist/v1";
+import NewSongForm from "./NewSongForm";
 
 const SongList = () => {
   const [songs, setSongs] = useState([
@@ -8,8 +9,8 @@ const SongList = () => {
     { title: "This wild darkness", id: 3 },
   ]);
 
-  const addSong = () => {
-    setSongs([...songs, { title: "new song", id: uuid() }]);
+  const addSong = (title) => {
+    setSongs([...songs, { title, id: uuid() }]);
   };
 
   return (
@@ -19,7 +20,7 @@ const SongList = () => {
           return <li key={song.id}>{song.title}</li>;
         })}
       </ul>
-      <button onClick={addSong}>Add Song</button>
+      <NewSongForm addSong={addSong} />
     </div>
   );
 };
